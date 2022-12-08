59613ad2cdf4317cd377b8b4f1824737f74db868
HP <HP@VASUOK>
2022-12-08 16:42:47 +0200

    x

--- WebCILab1/Program.cs
+++ WebCILab1/Program.cs
@@ -12,7 +12,7 @@ namespace WebCILab1
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {//
             CreateHostBuilder(args).Build().Run();
         }
 
